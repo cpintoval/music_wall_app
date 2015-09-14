a232dd40d37c615df41a8ecdaf1339fce52a590d
cpintoval <cpintovalderrama@gmail.com>
2015-09-14 17:24:35 +0000

    Adding form and detail song page

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,22 @@ get '/songs' do
   @songs = Song.all
   erb :'songs/index'
 end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  @song.save
+  redirect '/songs'
+end
+
+get '/songs/:id' do
+  @song = Song.find params[:id]
+  erb :'songs/show'
+end
